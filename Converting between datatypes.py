59f78542677161c36cfab0959c47f2e75c66dc3a
Maroti Haibate <47947677+Mario0126@users.noreply.github.com>
2022-05-02 01:03:03 +0530

    Add files via upload

---
+++ Converting between datatypes.py
@@ -0,0 +1,15 @@
+####V You can perform explicit datatype conversion.
+
+a = 1234343
+print(type(a))
+b = str(a)
+print (type(b))
+print(b)
+M = 'hello'
+N = list(M)
+print(type(N))
+print(N)
+O = set(M)
+print(O)
+P = tuple(M)
+print(P)
